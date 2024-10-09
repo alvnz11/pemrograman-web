9ad8bad4463dd9e17d6c216215035ef01a83d4df
Alvanza Saputra <alvanzasaputra123734@gmail.com>
2024-10-09 14:15:28 +0700

    pertemuan 6 percobaan awal

---
+++ Pertemuan06/dasarWeb/html_aman.php
@@ -0,0 +1,8 @@
+<?php
+$input = $_POST['input'];
+$input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+
+// $email = $_POST['email'];
+// if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//     # code...
+// }
